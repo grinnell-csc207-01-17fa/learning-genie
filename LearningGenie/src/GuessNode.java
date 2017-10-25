6e6ef628e8afb881880367014da9460df6e89249
Nannan Ji <jinannan@grinnell.edu>
2017-10-25 00:37:19 -0500

    Update

--- LearningGenie/src/GuessNode.java
+++ LearningGenie/src/GuessNode.java
@@ -8,13 +8,37 @@ public class GuessNode implements DecisionNode {
 	public DecisionNode left;
 	public DecisionNode right;
 	
+	public GuessNode(String guess){
+		this.guess = guess;
+		this.left = null;
+		this.right = null;
+	}
+	
 	public int countObjects() {
-		
+		return 0;
 	}
 	
 	public DecisionNode guess(Scanner in) {
-		
-		
+		System.out.println("Are you thinking of "+guess+" ?");
+		String answer = in.nextLine();
+		if(answer.equals("Yes")){
+			return this;
+		}else {
+			System.out.println("Oh no, I was wrong!");
+			System.out.println("What animal were you thinking of?");
+			String animal = in.nextLine();
+			System.out.println("What is a yes/no question that distinguishes a "+guess+" from a "+animal+"?");
+			String question = in.nextLine();
+			DecisionNode q =(DecisionNode) new QuestionNode(question, new GuessNode(animal), new GuessNode(this.guess));
+			return q;
+			
+			/*
+			System.out.println("Oh no, I was wrong!");
+			System.out.println("What animal were you thinking of?");
+			String animal = in.nextLine();
+			System.out.println("What is a yes/no question that distinguishes a Dog from a Cat?");
+		*/
+		}	
 	}
 	
 	public void write(FileWriter out) throws IOException {
--- LearningGenie/src/QuestionNode.java
+++ LearningGenie/src/QuestionNode.java
@@ -7,15 +7,49 @@ public class QuestionNode {
 	public String question;
 	public DecisionNode left;
 	public DecisionNode right;
+	public int guessNodes;
+	
+	public QuestionNode(String question, DecisionNode left, DecisionNode right){
+		this.question = question;
+		this.left = left;
+		this.right = right;
+	}
 	
 	public int countObjects() {
-		
+		return guessNodes;
 	}
 	
 	public DecisionNode guess(Scanner in) {
+		System.out.println(question);
+		String answer = in.nextLine();
+		if(answer.equals("Yes")){
+			if(this.left instanceof GuessNode){
+				DecisionNode left = this.left.guess(in);
+				this.left = left;
+				return left;
+			}else{
+				return this.left.guess(in);
+			}
+		}else {
+			if(this.right instanceof GuessNode){
+				DecisionNode right = this.right.guess(in);
+				this.right = right;
+				return right;
+			}else{
+				return this.right.guess(in);
+			}
+			/*
+			System.out.println("Oh no, I was wrong!");
+			System.out.println("What animal were you thinking of?");
+			String animal = in.nextLine();
+			System.out.println("What is a yes/no question that distinguishes a Dog from a Cat?");
+		*/
+		}
+		/*
+		System.out.println("Are you thinking of "+);
 		String answer = in.nextLine();
 		if
-		
+		*/	
 	}
 	
 	public void write(FileWriter out) throws IOException {
